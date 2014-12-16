d616ee37aaa66ca0218c8e995332f39c842ad8d5
zacsketches <zacsketches@github.com>
2014-12-16 09:13:49 -0800

    Merged attitude_computer from balance branch
      - I merged the attitude computer from the balance branch in
        order to clean up the code base and number of branches a little.
        Things were getting a little too interconnected.

--- blocks/attitude_computer.h
+++ blocks/attitude_computer.h
@@ -24,8 +24,49 @@
 #define INCLUDE_ATTITUDE_PRINT 1
 
 /*
-	Integrate a gyro and correct it with an error signal from an
-	accelerometer to publish the pitch message.
+(c) Zac Staples zacsketches (at) github.
+You can use this software as you see fit provided you 
+acknowledge the source.
+
+    Integrate a gyro and correct it with an error signal from an
+    accelerometer to publish the pitch message.
+
+    Use accelerometer pitch reading as negative feedback to the gyro 
+    pitch estimation with the intent of reducing the gyro drift error.
+
+    In Dec 2014, I updated the integration technique in this sketch
+    to use a custom adaptation of Simpson's rule to perform the 
+    numerical gyro integration instead of the simpler trapezoidal
+    integration.  My custom implementation of the Simpson rule
+    developed with Dirk Lundgren takes the 3/8 Simpson rule for integrting
+    over four datapoints and subtracts the standard 1/3 Simpson rule
+    for integrating over 3 points to give a result equal only to the dt width
+    slice from the 3rd to the 4th datapoint.  This slice is is accumulated
+    with each incoming datapoint to produce the gyro estimate.  See the two
+    links below for more on these two rules and the wikipedia page on
+    Simpsons rule for another explanation.  I have not found a reference that
+    analyzes the 4pt - 3pt technique I'm using here, but from the data I 
+    got in the gyro_drift_regulator.ino sketch in the MaryLou dev_sketches
+    folder I like the results this gives.
+    
+    http://mathworld.wolfram.com/Simpsons38Rule.html
+    http://mathworld.wolfram.com/SimpsonsRule.html
+    
+    I included a snapshot of the derivation for our use of Simpson's rule
+    in the references folder of the MaryLou bot.  See the parts in the
+    red annotated loops.
+    
+    In order to implement the Simpson integration I had to add a data
+    structure to the class for a First In First Out list.
+
+    See the PowerPoint brief in the datasheets section for the block
+    diagram implemented here.
+
+    DC_offset and Theta_bias from config file.
+    Values for gyro DC_offset and accel theta_bias were taken from
+    values collected in gyro_test.ino and accel_test.ino run multiple
+    times.  These values are a function of installation alignment and 
+    inherent sensor error.  
 	
     accel.enable default() results in the following sensor config:
       50hz
@@ -51,29 +92,118 @@
 
 */
 
-/*
-	TODO consider moving constant to static class data
-*/
 //Constants
-#define RAD_TO_DEG 57.2958  		// 180 / pi
-#define DEG_TO_RAD .0174533 		// pi / 180
 #define GYRO_COUNTS_TO_DEG .00875   // see above
 #define DEG_TO_GYRO_COUNTS 114.2857	// 1 / .00875
 #define TIMESCALE 1000.0            // use mills() for timing
 //#define TIMESCALE 1000000.0       // use mircos() for timing
 
+//provide default constants if there is no included robot config file.
+#ifndef THETA_BIAS
+    #define THETA_BIAS -0.43329    //FROM EXPERIMENTAL DATA WITH ACCEL_TEST.INO
+#endif
+#ifndef DC_OFFSET
+    #define DC_OFFSET -223         //From experimental data with gyro_test.ino
+#endif
+#ifndef PITCH_AXIS
+    #define PITCH_AXIS y    
+#endif
+#ifndef ROLL_AXIS
+    #define ROLL_AXIS x
+#endif
+#ifndef YAW_AXIS
+    #define YAW_AXIS z
+#endif
+#ifndef RAD_TO_DEG
+    #define RAD_TO_DEG 57.2958
+#endif
+#ifndef DEG_TO_RAD
+    #define DEG_TO_RAD .0174533
+#endif
+
 //*****************************************************************************
 //                               DATA STRUCTURES
 //*****************************************************************************
 
+typedef double Omega;       //Alias for angular velocity
+typedef double Epsilon;     //Alias for error
+typedef double Angle;       //Alias for angles
+
 struct Gyro_reading {
-    double    omega;
+    Omega    omega;
     Time timestamp;
 };
 
 struct Error {
-    double epsilon;
+    Epsilon epsilon;
     Time timestamp;
+};
+
+template<class T>
+struct Fifo_list{
+    T list[len];
+    int len;
+    int pointer;  //keep the pointer on the most recent data
+
+    Fifo_list(int list_length) 
+        : len(list_length), pointer(0)
+    { }
+
+    void add(const T val) {
+           //the pointer locates the position in the array where new data
+           //gets inserted
+        advance_pointer(pointer);
+        list[pointer] = val;
+    }
+
+    void advance_pointer(int& ptr) {
+        ++ptr;
+        if(ptr >= len) {
+            ptr = 0;
+        }   
+    }
+
+    void regress_pointer(int& ptr) {
+        --ptr;
+        if(ptr < 0) {
+            ptr = len-1;
+        }   
+    }
+
+    void data(T* copy) {
+       //fill the copy list with the current data.  I want a list that
+       //has the oldest data in position[0] with progressively newer
+       //data in subsequent locations
+        int dup_pointer = pointer;
+        for(int i=len-1; i<0; --i) {
+            copy[i]=list[dup_pointer];
+            regress_pointer(dup_pointer);   
+        }
+    }
+
+    double w0() {
+        int tmp_ptr = pointer;
+        regress_pointer(tmp_ptr);
+        regress_pointer(tmp_ptr);
+        regress_pointer(tmp_ptr);
+        return list[tmp_ptr].omega;
+    }
+
+    double w1() {
+        int tmp_ptr = pointer;
+        regress_pointer(tmp_ptr);
+        regress_pointer(tmp_ptr);
+        return list[tmp_ptr].omega;
+    }
+
+    double w2() {
+        int tmp_ptr = pointer;
+        regress_pointer(tmp_ptr);
+        return list[tmp_ptr].omega;
+    }
+    double w3() {
+        return list[pointer].omega;
+    }
 };
 
 //************************************************************************
@@ -97,21 +227,14 @@ public:
 		calibrated_gyro(gyroscope), 
 		calibrated_accel(accelerometer), 
 		gyro_plant(calibrated_gyro),
-		pub(&pitch_msg, &ch, local_msg)
-		
+		pub(&pitch_msg, &ch, local_msg)	
 	{}
 	
 	// From Base class
     //    char* name()
 	//    int id()
 	
-	void begin() {
-		//make sure Wire.begin() has been called 
-		if(gw::wire_begun == false){
-			Wire.begin();
-			gw::wire_begun = true;
-		}
-		
+	void begin() {	
 		// configure the PI controller
 		pi_controller.set_kp(default_kp);
 		pi_controller.set_ki(default_ki);
@@ -121,15 +244,15 @@ public:
 	    while(!calibrated_accel.begin());
 		
 		// set initial conditions
-	    double initial_condition = calibrated_accel.pitch_angle();
+	    Angle initial_condition = calibrated_accel.pitch_angle();
 	    theta_gyro = initial_condition;
 	    theta_accel = initial_condition;
 	}
 	
-	void set_kp(float kp) {pi_controller.set_kp(kp);}
-	void set_ki(float ki) {pi_controller.set_ki(ki);}
-	float kp() {return pi_controller.kp();}
-	float ki() {return pi_controller.ki();}
+	void set_kp(const float kp) {pi_controller.set_kp(kp);}
+	void set_ki(const float ki) {pi_controller.set_ki(ki);}
+	float kp() const {return pi_controller.kp();}
+	float ki() const {return pi_controller.ki();}
 	
 	/*
 		TODO change the constructor to attach the gyro and 
@@ -176,15 +299,15 @@ private:
 	Time now;
 	
 	//inputs to error computer
-	double theta_gyro;
-	double theta_accel;
+	Angle theta_gyro;
+    Angle theta_accel;
 	
 	//inputs to PI controller
 	Error error_signal; 
 	
 	//inputs to drift adjuster
 	Gyro_reading omega_raw;
-	double omega_adjustment;
+	Epsilon omega_adjustment;
 	
 	//inputs to gyro plant
 	Gyro_reading omega_compensated;
@@ -193,9 +316,9 @@ private:
 	gw::Publisher<Pitch_msg> pub;
 	Pitch_msg local_msg;
 		
-	// PI tuning based on an hour of tuning 10/16, but needs A LOT more
-	// engineering analysis for frequency, and step response with 
-	// an evaluation of stability and steady state error.
+	// Default values here represent a first hack to get things running.
+	// The robot should tune the gains to get the most stable response
+	// possible from the attitude computer.
 	static const double default_kp = 5.0;
 	static const double default_ki = 0.0;
 	
@@ -207,7 +330,10 @@ private:
 
 	class Calibrated_gyro {
 	public:
-	    Calibrated_gyro(L3G& gyroscope) : gyro(gyroscope) {}
+	    Calibrated_gyro(L3G& gyroscope) 
+	        : gyro(gyroscope), 
+	        pitch_axis(gyro.G.PITCH_AXIS) 
+	    {}
     
 	    bool begin() {
 	        bool result = false;
@@ -220,7 +346,7 @@ private:
 	    Gyro_reading read(){
 	        Gyro_reading theta_raw;
 	        gyro.read();
-	        theta_raw.omega = gyro.G.y-dc_offset;
+	        theta_raw.omega = pitch_axis - dc_offset;
 	        theta_raw.timestamp = millis();
         
 	        return theta_raw;
@@ -230,14 +356,19 @@ private:
     
 	private:
 	    L3G& gyro;
-	    static const double gyro_sensitivity = .00875;    //datasheet page 9.  mdeg/sec
-	    static const long   dc_offset = 126;       //units
+	    static const double gyro_sensitivity = GYRO_COUNTS_TO_DEG;    //datasheet page 9.  mdeg/sec
+	    static const long   dc_offset = DC_OFFSET;
+        float& pitch_axis;
           
 	} calibrated_gyro;
 
 	class Calibrated_accel {
 	public:
-	    Calibrated_accel(LSM303& accelerometer) : accel(accelerometer) {}
+	    Calibrated_accel(LSM303& accelerometer) 
+	        : accel(accelerometer),
+	        roll_axis(accel.A.ROLL_AXIS),
+	        yaw_axix(accel.A.YAW_AXIS)
+	    {}
     
 	    bool begin() {
 	        bool result = false;
@@ -247,24 +378,29 @@ private:
 	        return result;
 	    }
 
-	    double pitch_angle() {
-	        double theta_accel = 0.0;
+	    Angle pitch_angle() {
+	        Angle theta_accel = 0.0;
 	        accel.read();
 	        accel.shift_accel();
-	        float Gx = accel.A.x;
-	        float Gy = accel.A.y;
-	        float Gz = accel.A.z; 
-	        double tan_theta = -Gx / sqrt(pow(Gy, 2) + pow(Gz, 2));
+	        float Gy = roll_axis;
+	        float Gz = yaw_axis; 
+	        
+	        //use atan2 here so the full solution is solved in the range
+	        //[-180:180].  Since this attitude computer solves for rotation in
+	        //only one axis we don't need to worry about deciding which axis to 
+	        //constrain in the [-90:90] realm to eliminate the singularities
+	        //of the arctangent.  See datasheets and refs on this for more info.
+            theta_accel = atan2(Gy, Gz);
 
-	        theta_accel = atan(tan_theta);
 	        theta_accel *= RAD_TO_DEG;
-
 	        return theta_accel - theta_bias;
 	    }
     
 	private:
 	    LSM303& accel;
-	    static const float theta_bias =  -0.62306;
+	    static const Angle theta_bias = THETA_BIAS;
+        int16_t& roll_axis;     //using int16_t type because it's used in the LSM303 datasheet.
+        int16_t& yaw_axis;
 	} calibrated_accel;
 
 	class Drift_adjuster {
@@ -283,32 +419,60 @@ private:
 	public:
 	    Gyro_plant(Calibrated_gyro& gyroscope) : gyro(gyroscope)  {}
     
-	    double run(Gyro_reading omega_compensated) {
-	        //time management
-	        Time interval = omega_compensated.timestamp - timestamp_prev;
-	        double dt = (double)interval;
-	        dt = dt / timescale;
-	        timestamp_prev = omega_compensated.timestamp;
-        
-	        //find trapezoidal omega for integration
-	        double omega_mean = (omega_prev + omega_compensated.omega) / 2.0;
-        
-	        //perform numerical integration
-	        theta_counts += omega_mean * dt;
-        
-	        //get ready for the next run
-	        omega_prev = omega_mean;
-    
-	        theta_angle = theta_counts * gyro.conv_factor();      
-  
-	        return theta_angle;
-	    }
-    
+        Angle run(Gyro_reading omega_compensated) {
+            static double theta_counts;
+            double omega_prev;
+
+            Angle theta_angle = 0.0;
+    	        	    
+            //time management
+            Time interval = omega_compensated.timestamp - timestamp_prev;
+            double dt = (double)interval;
+            dt = dt / timescale;
+            timestamp_prev = omega_compensated.timestamp;
+
+            //find trapezoidal omega for integration
+            double omega_mean = (omega_prev + omega_compensated.omega) / 2.0;
+
+            //perform numerical integration
+            theta_counts += omega_mean * dt;
+
+            //get ready for the next run
+            omega_prev = omega_mean;
+
+            theta_angle = theta_counts * gyro.conv_factor();      
+
+            return theta_angle;
+        }
+
+        Angle run_simpson(Gyro_reading omega_compensated) {
+            static Fifo_list hist<Omega>(4);
+            double simpson_counts;
+            static double simpson_counts_prev;
+            
+            Angle simpson_angle = 0.0;
+
+            hist.add(omega_compensated.omega);
+
+            //time management
+            Time interval = omega_compensated.timestamp - timestamp_prev;
+            double dt = (double)interval;
+            dt = dt / timescale;
+            timestamp_prev = omega_compensated.timestamp;
+
+            //would be more accurate if I ignored the first four samples
+            simpson_counts = simpson_counts_prev + (9.0/24.0*hist.w3() + 19.0/24.0*hist.w2() - 5.0/24.0*hist.w1() + 1.0/24.0*hist.w0()) * dt;
+
+            simpson_angle = simpson_counts * gyro.conv_factor();
+
+            //get ready for the next run
+            simpson_counts_prev = simpson_counts;
+
+            return simpson_angle;
+        }
+
 	private:
 	    Calibrated_gyro& gyro;
-	    double theta_counts;
-	    double theta_angle;
-	    double omega_prev;
 	    Time timestamp_prev;
     
 	} gyro_plant;
@@ -317,7 +481,7 @@ private:
 	public:
 	    Error_computer() {}
     
-	    Error run(double theta_gyro, double theta_accel) {
+	    Error run(Angle theta_gyro, Angle theta_accel) {
 	        Error result;
 	        result.epsilon = theta_gyro - theta_accel;
 			result.epsilon *= DEG_TO_GYRO_COUNTS;
@@ -331,32 +495,61 @@ private:
 	public:
 	    PI_controller(double kp=0.0, double ki=0.0) :Kp(kp), Ki(ki) {}
 
-	    double run(Error input){
-	        double omega_adjustment = 0.0;
+        double run_parallel(Error input){
+            double omega_adjustment = 0.0;
+
+            // Time management
+            Time interval = input.timestamp - timestamp_prev;
+            double dt = (double)interval;
+            dt = dt / timescale;
+            timestamp_prev = input.timestamp;
+
+            //trap z integration for epsilon
+            epsilon_integrated += input.epsilon * dt;
+
+            //solve for adjustment
+            omega_adjustment = Kp * (input.epsilon) + Ki * (epsilon_integrated);
+
+            return omega_adjustment;
+        }
+
+        double run_standard(Error input) {
+            //see http://en.wikipedia.org/wiki/PID_controller for a discussion of standard form
+            //Ki is related to Ti by the expression
+            //         Kp
+            //  Ki = -------
+            //         Ti
+            //
+            //therefore, Ki/Kp = 1/Ti
+            static double one_over_Ti = Ki/Kp;
+
+            Epsilon omega_adjustment = 0.0;
+
+            // Time management
+            Time interval = input.timestamp - timestamp_prev;
+            double dt = (double)interval;
+            dt = dt / timescale;
+            timestamp_prev = input.timestamp;
+
+            //trap z integration for epsilon
+            epsilon_integrated += input.epsilon * dt;
+
+            //solve for adjustment
+            omega_adjustment = Kp * (input.epsilon +  one_over_Ti*epsilon_integrated);
+
+            return omega_adjustment;  
+        }
         
-	        // Time management
-	        Time dt = input.timestamp - timestamp_prev;
-	        dt /= timescale;
-        
-	        //integrate
-	        epsilon_integrated += input.epsilon * dt;
-        
-	        //solve for adjustment
-	        omega_adjustment = Kp * (input.epsilon) + Ki * (epsilon_integrated);
-        
-	        return omega_adjustment;
-	    }
-		
-		void set_kp(float kp) {Kp = kp;}
-		void set_ki(float ki) {Ki = ki;}
-		float kp() {return Kp;}
-		float ki() {return Ki;}
+		void set_kp(const float kp) {Kp = kp;}
+		void set_ki(const float ki) {Ki = ki;}
+		float kp() const {return Kp;}
+		float ki() const {return Ki;}
     
 	private:
 	    double Kp;
 	    double Ki;
 	    Time timestamp_prev;    
-	    double epsilon_integrated;
+	    Epsilon epsilon_integrated;
 	} pi_controller;
 	
 };
